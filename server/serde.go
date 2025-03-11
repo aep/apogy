53ad79c2412277a640d1a06112990483b7639f1d
Arvid E. Picciani <arvid@kraud.cloud>
2025-03-11 11:12:27 +0100

    database persistence is just json for now
    cant be bothered to fix msgpack encoding of json number.
    the actual performance difference seems irrelevant right now

--- server/serde.go
+++ server/serde.go
@@ -3,15 +3,26 @@ package server
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	openapi "github.com/aep/apogy/api/go"
 )
 
 func DeserializeStore(b []byte, doc *openapi.Document) error {
-	dec := json.NewDecoder(bytes.NewReader(b))
+	if len(b) < 1 {
+		return nil
+	}
+	if b[0] != 'j' {
+		return errors.New("invalid encoding stored in database")
+	}
+	dec := json.NewDecoder(bytes.NewReader(b[1:]))
 	dec.UseNumber()
 	return dec.Decode(doc)
 }
 
 func SerializeStore(doc *openapi.Document) ([]byte, error) {
-	return json.Marshal(doc)
+	b, err := json.Marshal(doc)
+	if err != nil {
+		return nil, err
+	}
+	return append([]byte{'j'}, b...), nil
 }
